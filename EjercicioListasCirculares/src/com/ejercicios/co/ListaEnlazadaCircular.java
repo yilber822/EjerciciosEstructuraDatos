154012f70a28cfac8cff8b765ab7f65da44c4b4d
DanielNempeque <daniel.nempeque@gmail.com>
2020-03-02 23:21:33 -0500

    Agregando comentarios

--- EjercicioListasCirculares/src/com/ejercicios/co/ListaEnlazadaCircular.java
+++ EjercicioListasCirculares/src/com/ejercicios/co/ListaEnlazadaCircular.java
@@ -24,17 +24,25 @@ public class ListaEnlazadaCircular {
 	public <E> void insertarEnCola(E dato) {
 		// TODO: Insertar en cola
 	}
-	
+	/**
+	 * @param <E>
+	 * @param dato is the value that will be the new head of the list
+	 */
 	public <E> void insertarEnCabeza(E dato) {
 		Nodo nodo = new Nodo(dato);
 		//check if the list is currently empty
 		if(checkEmpty()) {
+			//set the head and the tail as the new node
 			cabeza = nodo;
 			cola = nodo;
+			//set the next node of the head as itself
 			nodo.setSiguiente(cabeza);
 		}else {
+			//Sets the current head as the next value for the new head
 			nodo.setSiguiente(cabeza);
+			//sets the head as the new node
 			cabeza = nodo;
+			//sets the next value of the tail as the new head
 			cola.setSiguiente(cabeza);
 		}
 		
